630857540fc733d5ab2e00b0b94f507ba0bdfbc3
AlexC <55947391+AlexCards@users.noreply.github.com>
2021-01-10 12:33:45 +0300

    Created post component

--- src/api/useRequest.ts
+++ src/api/useRequest.ts
@@ -12,15 +12,15 @@ const initialState: State<null> = {
   error: null,
 };
 
-const useRequest = () => {
-  const state = ref(initialState);
+const useRequest = <res>() => {
+  const state = ref<State<res | null>>(initialState);
 
   const call = async (config: RequestInfo) => {
     try {
       state.value.isLoading = true;
 
       const response = await fetch(config);
-
+      
       state.value.response = await response.json();
     } catch (error) {
       state.value.error = error.message;
